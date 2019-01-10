71bd8360ca1a32023341456addbb4ac1e5cb99d5
康小广 <kenkangxgwe@gmail.com>
2019-01-09 21:41:51 -0500

    Add: more configuration in client, better way to start client
    New configurations:
    * wolfram kernel
    * wolfram language server folder
    
    Client starts socket by calling wolfram executable as if it is a node module.
    extension is simplified.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,37 +1,30 @@
 "use strict";
 
-import * as net from "net";
-
-import {Trace} from "vscode-jsonrpc";
-import { window, workspace, commands, ExtensionContext, Uri, Disposable, TextEditor } from "vscode";
+import { Trace } from "vscode-jsonrpc";
+import { workspace, ExtensionContext, Disposable, WorkspaceConfiguration } from "vscode";
 import {
     LanguageClient, LanguageClientOptions,
-    StreamInfo,
-    Position as LSPosition, Location as LSLocation,
-    ServerOptions
+    NodeModule,
+    TransportKind
 } from "vscode-languageclient";
 
 export function activate(context: ExtensionContext): void {
 
-    // clangd command
-    // const config = workspace.getConfiguration('WolframLanguageServer');
-    // // The server is a started as a separate app and listens on port 5007
-    // let wolfram: Executable = {
-    //     command: config.get<string>('wolfram.kernelPath')
-    //     // args: config.get<string>('arguments'),
-    // };
-    // let serverOptions: ServerOptions = wolfram;
-    let connectionInfo: net.NetConnectOpts = {
-        port: 6009
-    };
-    let serverOptions: ServerOptions = () => {
-        // connect to language server via socket
-        let socket: net.Socket = net.connect(connectionInfo);
-        let result: StreamInfo = {
-            writer: socket,
-            reader: socket
-        };
-        return Promise.resolve(result);
+    const config: WorkspaceConfiguration = workspace.getConfiguration("WolframLanguageServer");
+    let wolframkernel: string = config.get<string>("WolframKernelPath");
+    let wlServerDir: string = config.get<string>("WLServerPath");
+    let socketport: number = Number(config.get<number>("Port"));
+    let serverOptions: NodeModule = {
+        module: wlServerDir + "init.wls",
+        runtime: wolframkernel,
+        args: [],
+        transport: {
+            kind: TransportKind.socket,
+            port: socketport
+        },
+        options: {
+            execArgv: ["-script"]
+        }
     };
 
     let clientOptions: LanguageClientOptions = {
@@ -42,24 +35,11 @@ export function activate(context: ExtensionContext): void {
     };
 
     // create the language client and start the client.
-    let client:LanguageClient = new LanguageClient("WolframLanguageServer", "Wolfram Language Server", serverOptions, clientOptions);
-
-    // var disposable2:Disposable =commands.registerCommand("mydsl.a.proxy", async () => {
-    //     let activeEditor:TextEditor = window.activeTextEditor;
-    //     if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== "mydsl") {
-    //         return;
-    //     }
-
-    //     if (activeEditor.document.uri instanceof Uri) {
-    //         commands.executeCommand("mydsl.a", activeEditor.document.uri.toString());
-    //     }
-    // });
-
-    // context.subscriptions.push(disposable2);
+    let client: LanguageClient = new LanguageClient("WolframLanguageServer", "Wolfram Language Server", serverOptions, clientOptions);
 
     // enable tracing (.Off, .Messages, Verbose)
-    client.trace = Trace.Verbose;
-    let disposable:Disposable = client.start();
+    client.trace = Trace.fromString(config.get<string>("trace.server")); // Trace.Verbose;
+    let disposable: Disposable = client.start();
 
     // push the disposable to the context's subscriptions so that the
     // client can be deactivated on extension deactivation
